d7e986a4ef17a2453471092bf31f56124686cd72
cperez1012 <50646333+cperez1012@users.noreply.github.com>
2023-11-09 14:22:33 -0500

    incorporated async and await to function in script.js and to mimic grabbing data to populate the list for task and category.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function addTask() {
   let taskInput = document.getElementById('new-task');
+  let categorySelect = document.getElementById('task-category');
   let taskList = document.getElementById('task-list');
 
-  if(taskInput.value.trim() !== '') {
+  let taskValue = taskInput.value.trim();
+  let categoryValue = categorySelect.value;
+
+  // if(taskInput.value.trim() !== '') {
+  if(taskValue !== '') {
     let listItem = document.createElement('li');
-    listItem.textContent = taskInput.value.trim();
+    listItem.textContent = 
+    `${taskValue} - Category: ${categoryValue}`;
     taskList.appendChild(listItem);
     taskInput.value = '';
+    categorySelect.selectedIndex = 0;
+
+    listItem.addEventListener('click', () => toggleTaskComplete(listItem));
   }
 
   list.addEventListener('click', () => toggleTaskComplete(listItem));
@@ -15,3 +24,94 @@ function addTask() {
 function toggleTaskComplete(taskItem) {
   taskItem.classList.toggle('completed');
 }
+
+const fetchTasks = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['Task 1', 'Task 2', 'Task 3']);
+    }, 1000);
+  });
+}
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   fetchTasks().then(tasks => {
+//     tasks.forEach(task => addTaskToDOM(task));
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', async (event) => {
+  try {
+    const tasks = await fetchTasks();
+    tasks.forEach(task => addTaskToDOM(task));
+  } catch (error) {
+    console.error('Failed to fetch tasks: ', error);
+  }
+});
+
+const addTaskToDOM = (taskContent, taskCategory) => {
+  let taskList = document.getElementById('task-list');
+  let listItem = document.createElement('li');
+  // listItem.textContent = taskContent;
+  listItem.textContent = `${taskContent} - Category: ${taskCategory}`;
+  taskList.appendChild(listItem);
+}
+
+// const saveTask = (taskContent) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('Task saved:', taskContent);
+//       resolve(taskContent);
+//     }, 500);
+//   });
+// }
+
+const saveTask = async (taskContent, taskCategory) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Task saved:', taskContent, 'Category: ', taskCategory);
+      resolve({ content: taskContent, category: taskCategory });
+    }, 500);
+  });
+};
+
+// const handleAddTask = () => {
+//   let taskInput = document.getElementById('new-task');
+//   let taskContent = taskInput.value.trim();
+
+//   if(taskContent) {
+//     saveTask(taskContent).then(task => {
+//       addTaskToDOM(task);
+//       taskInput.value = '';
+//     }).catch(error => {
+//       console.error('Failed to save task:', error);
+//     });
+//   }
+// }
+
+const handleAddTask = async () => {
+  let taskInput = document.getElementById('new-task');
+  let categorySelect = document.getElementById('task-category');
+  let taskContent = taskInput.value.trim();
+  let taskCategory = categorySelect.value;
+
+  if(taskContent) {
+    try {
+      const task = await saveTask(taskContent, taskCategory);
+      addTaskToDOM(task, taskCategory);
+      taskInput.value = '';
+      categorySelect.selectedIndex = 0;
+    }catch (error) {
+      console.error('Failed to save task: ', error);
+    }
+  }
+};
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const addButton = document.getElementById('add-task-btn');
+//   addButton.addEventListener('click', handleAddTask);
+// });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const addButton = document.getElementById('add-task-btn');
+  addButton.addEventListener('click', () => handleAddTask());
+});
